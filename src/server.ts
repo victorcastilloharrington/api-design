0798b248b45ff80f8b25f846f3888b742923bedb
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2024-10-15 11:37:09 -0700

    feat(api): added required middlewares

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 export default app;

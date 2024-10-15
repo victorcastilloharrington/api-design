e367c3f43b06d1af2a72c62bf07c2d3368996162
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2024-10-15 11:15:34 -0700

    feat(api): initialized router

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 // src/server.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import router from "./router";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use('/api', router)
 
 export default app

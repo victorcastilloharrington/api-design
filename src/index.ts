e1eb7e169a0b04abe3f5eefd73b169406d45dc5f
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2024-10-15 10:52:10 -0700

    feat(db): initialized prisma

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 // src/index.ts
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import app from './server';
 
-dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running asd at http://localhost:${port}`);
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// src/server.ts
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+export default app
